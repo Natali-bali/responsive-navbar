fda05d64b0760732a6fdb2a15e4c9f8ffef09c02
Natalia Konovalova <ko.natalia.de@gmail.com>
2020-11-29 09:27:42 +0100

    changed html deleted div with second navbar for mobile menu

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let myFunction = function() {
     document.querySelector('.menu-icon__lines').classList.toggle('menu-icon__cross');
-    document.querySelector('#mobile-menu').classList.toggle('mobile-menu__active');
+    document.querySelector('#navbar').classList.toggle('mobile-menu__active');
 }
 let menuIcon = document.querySelector('#menu-icon');
   menuIcon.onclick =  myFunction;
